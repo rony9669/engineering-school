cb059bccf416af8a17357af30dceb59393abc8ea
rony9669 <ronychowdhury.cse@gmail.com>
2022-09-29 23:21:13 +0600

    break time added

--- src/Componetnts/Cart/Cart.js
+++ src/Componetnts/Cart/Cart.js
@@ -22,7 +22,12 @@ const Cart = (props) => {
       <div>
         <MyInfo></MyInfo>
       </div>
-      <div></div>
+      <div>
+        <BreakTime
+          handleClick={handleClick}
+          handleAddToCartClick={handleAddToCartClick}
+        ></BreakTime>
+      </div>
       <div>
         <h3 className="reading-summary">Reading summary</h3>
         <div className="reading-time">
